181782a0ec6d24796117dddda71875d524793db8
Nie ShiQiang <nsqiang@gmail.com>
2022-11-27 14:22:03 +0800

    注释掉一些导致能跑

--- set_hugepages-sw.sh
+++ set_hugepages-sw.sh
@@ -1,5 +1,5 @@
 mkdir -p /dev/hugepages
 mountpoint -q /dev/hugepages || mount -t hugetlbfs nodev /dev/hugepages
-echo 512 >/sys/devices/system/node/node0/hugepages/hugepages-8192kB/nr_hugepages
-
+echo 2048 >/sys/devices/system/node/node0/hugepages/hugepages-8192kB/nr_hugepages
+echo 2048 >/sys/devices/system/node/node1/hugepages/hugepages-8192kB/nr_hugepages
 echo "大页设置成功"
